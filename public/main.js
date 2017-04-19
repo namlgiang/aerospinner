bb1928db53b37073775d561b0d67a3047a580875
Giang Nam <namle@kinssolutions.net>
2017-04-19 23:24:23 +0700

    asd

--- public/main.js
+++ public/main.js
@@ -1,3 +1,12 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-44699912-31', 'auto');
+ga('require', 'displayfeatures');
+ga('send', 'pageview');
+
 var color = 0;
 var quantity = 1;
 var colorName = ["Silver", "Rose Gold", "Spring", "Midnight", "Ocean"];
@@ -111,6 +120,8 @@ paypal.Button.render({
     onAuthorize: function(data, actions) {
 
         // Optional: display a confirmation page here
+        ga('send', 'pageview', 'InitiateCheckout');
+        console.log('InitiateCheckout');
 
         return actions.payment.execute().then(function() {
             window.location.href = "/thankyou/";
