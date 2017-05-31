f846bd76e333afb32a6293e6f2826735c07ff6ea
Giang Nam <namle@kinssolutions.net>
2017-05-31 23:33:06 +0700

    asd

--- index.js
+++ index.js
@@ -69,6 +69,8 @@ app.use(express.static('public'));
 app.get('/coupon/:code', function(req, res) {
   if(req.params.code.toUpperCase() == "SECRET60")
     res.send("1");
+  else if(req.params.code.toUpperCase() == "SPECIAL30")
+    res.send("2");
   else
     res.send("0");
 });
